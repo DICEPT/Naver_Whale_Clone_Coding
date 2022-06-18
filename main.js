defda1c0db4cef8f474c0e95cf57dbad5a994ef8
DiMAC <dimac@iMac.local>
2022-06-18 20:25:15 +0900

    커밋i

---
+++ main.js
@@ -0,0 +1,50 @@
+
+// main_tasking 영역
+
+document.querySelector('.main_tasking input[id="slide01"]').addEventListener('click', function () {
+    document.querySelector('.main_tasking .inner_wide').style.transform = 'translate(0vw)';
+});
+
+document.querySelector('.main_tasking input[id="slide02"]').addEventListener('click', function () {
+    document.querySelector('.main_tasking .inner_wide').style.transform = 'translate(-100%)';
+});
+
+document.querySelector('input[id="slide03"]').addEventListener('click', function () {
+    document.querySelector('.main_tasking .inner_wide').style.transform = 'translate(-200%)';
+});
+
+document.querySelector('input[id="slide04"]').addEventListener('click', function () {
+    document.querySelector('.main_tasking .inner_wide').style.transform = 'translate(-300%)';
+});
+
+document.querySelector('input[id="slide05"]').addEventListener('click', function () {
+    document.querySelector('.main_tasking .inner_wide').style.transform = 'translate(-400%)';
+});
+
+// main_security 영역
+
+document.querySelector('.main_security input[id="slide01"]').addEventListener('click', function () {
+    document.querySelector('.main_security .inner_wide').style.transform = 'translate(0vw)';
+});
+
+document.querySelector('.main_security input[id="slide02"]').addEventListener('click', function () {
+    document.querySelector('.main_security .inner_wide').style.transform = 'translate(-100%)';
+});
+
+document.querySelector('.main_security input[id="slide03"]').addEventListener('click', function () {
+    document.querySelector('.main_security .inner_wide').style.transform = 'translate(-200%)';
+});
+
+// main_custom 영역
+
+document.querySelector('.main_custom input[id="slide01"]').addEventListener('click', function () {
+    document.querySelector('.main_custom .inner_wide').style.transform = 'translate(0vw)';
+});
+
+document.querySelector('.main_custom input[id="slide02"]').addEventListener('click', function () {
+    document.querySelector('.main_custom .inner_wide').style.transform = 'translate(-100%)';
+});
+
+document.querySelector('.main_custom input[id="slide03"]').addEventListener('click', function () {
+    document.querySelector('.main_custom .inner_wide').style.transform = 'translate(-200%)';
+});
